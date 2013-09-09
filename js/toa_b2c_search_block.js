77a8240c870e3bbb8efba06363103e0e58650f1e
Travel Open Apps Developer <toa.dev@gmail.com>
2013-09-09 17:15:26 +0200

    Adding toa search block implementation

--- js/toa_b2c_search_block.js
+++ js/toa_b2c_search_block.js
@@ -1,5 +1,22 @@
 /**
  * @file 
- * this file conteins the necessary javascript for TOA search block.
+ * This file conteins the necessary javascript for TOA search block.
  */
 
+var funcionCallback = function (data) {
+	jQuery('#searcher').whiteSearcher(data);
+};
+
+var loadSearcher = function () {
+	var url = "http://reservas.emergya.es/searcher.htt";
+	var params = {
+	  callback: function(data){ funcionCallback(data);},
+	  callbackParamName: "searcherCallback",
+	  params: {}
+	};
+	jQuery.jsonp.call(url, params);
+};
+
+jQuery(document).ready(function() {
+  loadSearcher();
+});
